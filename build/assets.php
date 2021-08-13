e494fbe58f8a4f5757a6a783b3271de34f13526f
Daniel Mejta <daniel@mejta.net>
2021-08-13 13:39:42 +0200

    Fix npm

---
+++ build/assets.php
@@ -0,0 +1,1 @@
+<?php return array('settings.js' => array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '41e2b514dce7a63f24fd514d7cb13d98'));
