07d44ef7b345fc0014f4dc4e453e414da5114c76
vasikgreif <info@vaclavgreif.cz>
2021-09-10 08:51:27 +0200

    WIP

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-
-module.exports = {
-    ...defaultConfig,
-    entry: {
-        'settings': ['./assets/admin/settings.js', './assets/admin/settings.scss'],
-    }
-};
