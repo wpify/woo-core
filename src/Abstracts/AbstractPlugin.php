0486ea3f6be0c1159f9692ad4090017b5b3f741d
Martin Svoboda <martin@wpify.io>
2025-01-28 17:46:21 +0100

    Add AbstractPlugin

---
+++ src/Abstracts/AbstractPlugin.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Wpify\WooCore\Abstracts;
+
+use Wpify\PluginUtils\PluginUtils;
+use Wpify\WooCore\Admin\Settings;
+use Wpify\WooCore\WooCommerceIntegration;
+use Wpify\WooCore\WpifyWooCore;
+
+/**
+ * Class AbstractModule
+ * @package WpifyWoo\Abstracts
+ */
+abstract class AbstractPlugin {
+	private WpifyWooCore $wpify_woo_core;
+	private PluginUtils $plugin_utils;
+
+
+	public function __construct(
+		WpifyWooCore $wpify_woo_core,
+		PluginUtils $plugin_utils,
+	) {
+		$this->wpify_woo_core = $wpify_woo_core;
+		$this->plugin_utils   = $plugin_utils;
+
+		add_filter( 'wpify_installed_plugins', array( $this, 'add_plugin' ) );
+		add_filter( 'plugin_action_links_' . $this->id() . '/' . $this->id() . '.php', array(
+			$this,
+			'add_action_links'
+		) );
+		add_filter( 'plugin_row_meta', array( $this, 'add_row_meta_links' ), 10, 2 );
+	}
+
+	/**
+	 * Plugin id
+	 *
+	 * @return string
+	 */
+	public function id(): string {
+		return $this->plugin_utils->get_plugin_slug();
+	}
+
+	/**
+	 * Plugin name
+	 *
+	 * @return string|null
+	 */
+	public function name(): ?string {
+		return $this->plugin_utils->get_plugin_name();
+	}
+
+	/**
+	 * Plugin settings url
+	 *
+	 * @return string
+	 */
+	public function settings_url(): string {
+		return add_query_arg( [ 'page' => sprintf( 'wpify/%s', $this->id() ) ], admin_url( 'admin.php' ) );
+	}
+
+	/**
+	 * Plugin documentation url
+	 *
+	 * @return string
+	 */
+	public function documentation_url(): string {
+		return '';
+	}
+
+	/**
+	 * Register plugin into WPify dashboard
+	 *
+	 * @param $plugins
+	 *
+	 * @return mixed
+	 */
+	public function add_plugin( $plugins ) {
+		$plugins[ $this->id() ] = array_merge(
+			array(
+				'settings' => $this->settings_url(),
+			),
+			$this->plugin_utils->get_plugin_info()
+		);
+
+		return $plugins;
+	}
+
+	/**
+	 * Add action links to WP plugin list
+	 *
+	 * @param $links
+	 *
+	 * @return array
+	 */
+	public function add_action_links( $links ): array {
+		$before = array(
+			'settings' => sprintf( '<a href="%s">%s</a>', $this->settings_url(), __( 'Settings', 'wpify-woo' ) ),
+		);
+
+		$after = array(
+			'wpify' => sprintf( '<a href="%s" target="_blank">%s</a>', 'https://wpify.io', __( 'Get more plugins and support', 'wpify-woo' ) ),
+		);
+
+		return array_merge( $before, $links, $after );
+	}
+
+	/**
+	 * Add action links on WP plugin list into meta
+	 *
+	 * @param $plugin_meta
+	 * @param $plugin_file
+	 *
+	 * @return array
+	 */
+	public function add_row_meta_links( $plugin_meta, $plugin_file ): array {
+		$new_links = array();
+
+		if ( strpos( $plugin_file, $this->plugin_utils->get_plugin_file() ) ) {
+			$new_links = array(
+				'wpify-doc' => sprintf( '<a href="%s" target="_blank">%s</a>', $this->documentation_url(), __( 'Documentation', 'wpify-woo' ) ),
+			);
+		}
+
+		return array_merge( $plugin_meta, $new_links );
+	}
+
+	/**
+	 * After activate plugin action
+	 *
+	 * @return void
+	 */
+	public function activate() {
+	}
+
+	/**
+	 * After deactivate plugin action
+	 *
+	 * @return void
+	 */
+	public function deactivate() {
+	}
+
+	/**
+	 * After uninstall plugin action
+	 *
+	 * @return void
+	 */
+	public function uninstall() {
+	}
+}
